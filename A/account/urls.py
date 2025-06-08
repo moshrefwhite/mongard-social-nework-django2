b4bd0740a75e2c19cd94acd30b372cddb8f11f47
moshrefwhite <ms19951415@gmail.com>
2025-06-08 16:44:41 +0330

    costomize registrationform and solved some errors in views and forms

--- A/account/urls.py
+++ A/account/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='user_register'),
     
-    
-    
 ]
--- A/home/views.py
+++ A/home/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
-def HomeView(self, request):
+class HomeView(View):
     def get(self, request):
         return render(request, 'home/index.html')
     
     def post(self, request):
         return render(request, 'home/index.html')
         
-    
-    
+
     
     
     
