1b2e40120aab0194068d7a8195b3c726415238de
moshrefwhite <ms19951415@gmail.com>
2025-06-08 11:37:14 +0330

    start using class base view and View class

--- A/home/views.py
+++ A/home/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .views import View
 
-def home(request):
-    return render(request, 'home/index.html')
+def HomeView(self, request):
+    def get(self, request):
+        return render(request, 'home/index.html')
+    
+    def post(self, request):
+        return render(request, 'home/index.html')
+        
+    
+    
+    
+    
+    
