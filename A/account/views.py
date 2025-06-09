d5783738a1abfbba91c96dcd293917bb9cb2590f
moshrefwhite <ms19951415@gmail.com>
2025-06-09 18:29:00 +0330

    add:login class view, create: login html file, add: login url and button, add: login form

--- A/account/views.py
+++ A/account/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 class RegisterView(View):
   form_class = UserRegistrationForm
@@ -20,3 +22,24 @@ class RegisterView(View):
       messages.success(request, 'you registered successfully', 'success')
       return redirect('home:home')
     return render(request, self.template_name, {'form': form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'username or password is wrong', 'warning')
+        return render(request, self.template_name, {'form':form})
+
+    
