b087c6edc2abf499fce961b4aa1f3afd44f3c4e3
moshrefwhite <ms19951415@gmail.com>
2025-06-10 15:57:16 +0330

    add logout urls refactor: navbor.html file, authenticate login, register and user login

--- A/account/urls.py
+++ A/account/urls.py
@@ -5,5 +5,6 @@ app_name = 'account'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='user_register'),
     path('login/', views.UserLoginView.as_view(), name='user_login'),
-    
+    path('logout', views.UserLogoutView.as_view(), name='user_logout'),
+
 ]
